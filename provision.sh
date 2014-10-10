d2a14c2d8ab36675d62be959cb9a9c487ce4bf59
Ian Sinnott <ian@iansinnott.com>
2014-10-10 21:03:39 +0800

    Update provisioning file so that it actually works.

--- provision.sh
+++ provision.sh
@@ -1,2 +1,21 @@
-export SUPER_BOOSH="Totally loaded the shell script."
-echo "Hey bro, nice terminal."
+# We'll use a modified version of the official docker install script. I was
+# running into an issue where docker wouldn't be installed without the
+# --force-yes option.
+#
+# The original script can be found at: https://get.docker.io/ubuntu/
+
+# Check that HTTPS transport is available to APT
+if [ ! -e /usr/lib/apt/methods/https ]; then
+	sudo apt-get -y update
+	sudo apt-get -y install apt-transport-https
+fi
+
+# Add the repository to your APT sources
+sudo echo deb https://get.docker.com/ubuntu docker main > /etc/apt/sources.list.d/docker.list
+
+# Then import the repository key
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+
+# Install docker
+sudo apt-get -y update
+sudo apt-get -y --force-yes install lxc-docker
